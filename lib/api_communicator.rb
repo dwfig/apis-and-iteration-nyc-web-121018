9e22ebb68ae74856df884c1618850d16119cb5aa
Daniel Fries <daniel.w.fries@gmail.com>
2018-12-26 20:26:31 -0500

    barebones finished

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,20 +6,48 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  #binding.pry
+  character_movies = []
+  response_hash["results"].each do |character|
+  #  binding.pry
+     if character["name"].downcase === character_name
+       #binding.pry
+       character["films"].each do |movie|
+         character_movies << JSON.parse(RestClient.get(movie))
+         #binding.pry
+       end
+     end
+   end
+
+
+
+
+## "Luke Skywalker" = response_hash["results"][0]["name"]
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
+
+
+
+
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+
+  ##binding.pry
+  character_movies
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.map do |movie|
+    #binding.pry
+    puts movie["title"]
+  end
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  "Hello! And welcome to the Star Wars API Ruby app."
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  gets.chomp.downcase
 end
